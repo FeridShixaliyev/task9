3075848fd74d4506004b7b3d6501b08ff44247e3
FeridShixaliyev <tu7b9ed4n@code.edu.az>
2022-03-22 22:53:02 +0400

    task

---
+++ Vehicle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp28
+{
+     class  Vehicle
+    {
+        public string Color;
+        public string Brand;
+        public int Millage;
+        
+        public virtual string ShowInfo
+        {
+            get 
+
+        }
+        public abstract Drive(double millage)
+        {
+            return
+        }
+
+    }
+}
