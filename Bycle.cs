06755c6402b11da505c40e2d2fce0cc110718b2a
FeridShixaliyev <99806164+FeridShixaliyev@users.noreply.github.com>
2022-03-24 00:39:21 +0400

    task1

---
+++ Bycle.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp28
+{
+    class Bycle:Vehicle
+    {
+        public override void Drive(double millage)
+        {
+            Millage = Millage + millage;
+        }
+    }
+}
