f9d9462d56477926bf626369b38ba924ac1215a7
FeridShixaliyev <99806164+FeridShixaliyev@users.noreply.github.com>
2022-03-24 00:38:23 +0400

    task1

---
+++ Vehicle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp28
+{
+    abstract class Vehicle
+    {
+        public string Color { get; set; }
+        public string Brand { get; set; }
+        public double Millage { get; set; }
+
+        public virtual void ShowInfo()
+        {
+            Console.WriteLine($"Color-{Color}\nBrand{Brand}\nMillage{Millage}");
+        }
+        public abstract void Drive(double millage);
+        
+    }
+}
