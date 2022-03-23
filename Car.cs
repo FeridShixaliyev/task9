1cc2474cb4bce48eb226e5fc36f9f50c2fa6eb12
FeridShixaliyev <99806164+FeridShixaliyev@users.noreply.github.com>
2022-03-24 00:38:55 +0400

    task1

---
+++ Car.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp28
+{
+    class Car: Vehicle
+    {
+        public int FuelCapacity;
+        public double CurrentFuel;
+        public double FuelFor1km;
+
+        public override void Drive(double millage)
+        {
+            if (millage * FuelFor1km <= CurrentFuel)
+            {
+                Console.WriteLine("Yeteri qeder benzin var");
+                CurrentFuel = CurrentFuel - millage * FuelFor1km;
+                Millage = Millage + millage;
+                Console.WriteLine($"Gedilecek yol sonrasi benzin:{CurrentFuel}");
+
+            }
+            else 
+            {
+                Console.WriteLine("Yeteri qeder benzin yoxdur!!");
+            }
+        }
+
+    }
+}
