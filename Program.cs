92c807726090328754165560880cc5dd07b2b04e
FeridShixaliyev <99806164+FeridShixaliyev@users.noreply.github.com>
2022-03-24 00:37:27 +0400

    task1

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApp28
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car[] car;
+            
+
+            Console.WriteLine("Nece eded avtomobil var:");
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+
+            }
+            
+                
+           
+
+
+        }
+    }
+}
