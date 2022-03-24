06379e31b190d34e40698847aaf3cb30ada407e6
FeridShixaliyev <99806164+FeridShixaliyev@users.noreply.github.com>
2022-03-24 11:58:30 +0400

    task2

---
+++ Program2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp29
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User user = new User();
+            user.Fullname = "Ferid Sixaliyev";
+            user.Email = "ferid-sixaliyev@mail.ru";
+            
+            user.ShowInfo();
+            Console.WriteLine($"Password checking...{ user.PasswordChecker("Ferid1524")}");
+
+
+        }
+    }
+}
